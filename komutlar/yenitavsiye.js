6f82d92e24d084747dbfce3a47c84c085d4773c9
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-11-17 18:09:46 +0300

    Update yenitavsiye.js

--- komutlar/yenitavsiye.js
+++ komutlar/yenitavsiye.js
@@ -18,42 +18,16 @@ exports.run = (client, message, args) => {
     .setColor('RANDOM')
     .setTitle('Tavsiye Bilgileri')
     .addField('Tavsiye:', mesaj, true)
-    .addField('Kullanıcı Adı:', message.author.tag, true)
-    .addField('Kullanıcı Kimliği:', message.author.id, true)
-    .addField('Sunucu Adı:', message.guild.name, true)
-    .addField('Sunucu Kimliği:', message.guild.id, true)
-    .addField('Sunucu Daveti:', davetlinki.url)
+    .addField('✭ Kullanıcı Adı:', message.author.tag, true)
+    .addField('✭ Kullanıcı Kimliği:', message.author.id, true)
+    .addField('✭ Sunucu Adı:', message.guild.name, true)
+    .addField('✭ Sunucu Kimliği:', message.guild.id, true)
+    .addField('✭ Sunucu Daveti:', davetlinki.url)
     client.fetchUser(id)
     .then(user => {user.send({embed})})
   }
 });
-
-
-if (!message.guild) {
-const ozelmesajuyari = new Discord.RichEmbed()
-  .setColor("#ffffff")
-  .setTimestamp()
-  .setAuthor(message.author.username, message.author.avatarURL)
-  .addField('`tavsiye` komutu sunucularda kullanılabilir!')
-return message.author.sendEmbed(ozelmesajuyari); }
-let guild = message.guild
-let mesaj = args.slice(0).join(' ');
-  if (mesaj.length < 1) return message.channel.send(new Discord.RichEmbed().setColor("RED").setDescription(':no_entry_sign: :pencil: Tavsiye için bana birşey yazman gerek!'));
-  const tavsiye1 = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setDescription(message.author.username + ', :white_check_mark: Tavsiyeniz bildirildi! Tavsiyenizi bildirdiğiniz için teşekkür ederiz!')
-  message.channel.send(tavsiye1);
 /*let TavsiyeKanalı = bot.guilds.get("511144954836549658").channels.get("513347194477346826");*/
-const tavsiye = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setThumbnail(message.author.avatarURL)
-  .setDescription(':newspaper2: **' + message.author.tag + '** adlı kullanıcının önerisi;')
-  .addField(':envelope: Kullanıcı Bilgileri', '✭ ID: ' + message.author.id + '\n✭ Adı: ' + message.author.username + '\n✭ Tagı: ' + message.author.discriminator + '')
-  .addField(':pencil: Tavsiye', mesaj)
-return bot.channels.get("274551537139712001").send(tavsiye);
-  const Davet = bot.channels.get(message.channel.id).createInvite({ maxAge: 0})
-  bot.channels.get("274551537139712001").send(Davet.url)
-};
 
 exports.conf = {
   enabled: true,
