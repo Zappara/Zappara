32a9fb38b60d35a516e82b3aed724bcdfbb0ca20
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-11-17 16:38:51 +0300

    Create yenitavsiye.js

---
+++ komutlar/yenitavsiye.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+if (!message.guild) {
+const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor("#ffffff")
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField('`tavsiye` komutu sunucularda kullanılabilir!')
+return message.author.sendEmbed(ozelmesajuyari); }
+let guild = message.guild
+let mesaj = args.slice(0).join(' ');
+  if (mesaj.length < 1) return message.channel.send(new Discord.RichEmbed().setColor("RANDOM").setDescription(':no_entry_sign: :pencil: Tavsiye için bana birşey yazman gerek!'));
+  const tavsiye1 = new Discord.RichEmbed()
+    .setColor("RANDOM")
+    .setDescription(message.author.username + ', :basarili:  Tavsiyeniz bildirildi! Tavsiyenizi bildirdiğiniz için teşekkür ederiz!')
+  message.channel.send(tavsiye1);
+const Davet = bot.channels.get(message.channel.id).createInvite({ maxAge: 0})
+const tavsiye = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
+  .setDescription(':newspaper2: **' + message.author.tag + '** adlı kullanıcının önerisi;')
+  .addField(':envelope: Kullanıcı Bilgileri', '✭ ID: ' + message.author.id + '\n✭ Adı: ' + message.author.username + '\n✭ Tagı: ' + message.author.discriminator + '')
+  .addField(':pencil: Tavsiye', mesaj)
+  .addField(':link: Davet', Davet)
+return client.channels.get("513347194477346826").send(tavsiye);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'tavsiye',
+  description: 'Bota eklenmesini istediğiniz şeyi tavsiye etmenizi sağlar',
+  usage: 'tavsiye [tavsiye]'
+};
