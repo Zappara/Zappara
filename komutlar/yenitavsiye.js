0fd18decb1d4e54a52fc2aefd22913b4bad153ce
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-11-17 18:05:37 +0300

    Update yenitavsiye.js

--- komutlar/yenitavsiye.js
+++ komutlar/yenitavsiye.js
@@ -1,6 +1,34 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(' ')[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(' ').slice(1);
+  const davetlinki = bot.channels.get(message.channel.id).createInvite({ maxAge: 0})
+  if (command === 'tavsiyeni-gönder' || command === 'tavsiye') {
+    let str = '<@274551537139712001>';//@silmeyin!
+    let id = str.replace(/[<@!>]/g, '');
+    let mesaj = args.slice(0).join(' ');
+    if (mesaj.length < 1) return message.channel.send(new Discord.RichEmbed().setColor("RED").setDescription(':no_entry_sign: :pencil: Tavsiye için bana birşey yazman gerek!'));
+    const embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setTitle('Tavsiye Bilgileri')
+    .addField('Tavsiye:', mesaj, true)
+    .addField('Kullanıcı Adı:', message.author.tag, true)
+    .addField('Kullanıcı Kimliği:', message.author.id, true)
+    .addField('Sunucu :', message.guild.name, true)
+    .addField('Sunucu kimliği:', message.guild.id, true)
+    .addField('Sunucu daveti:', davetlinki.url)
+    client.fetchUser(id)
+    .then(user => {user.send({embed})})
+  }
+});
+
+
 if (!message.guild) {
 const ozelmesajuyari = new Discord.RichEmbed()
   .setColor("#ffffff")
