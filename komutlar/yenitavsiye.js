93d4650e04267927dcde8b7608033b26860e5cfe
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-11-17 17:56:20 +0300

    Update yenitavsiye.js

--- komutlar/yenitavsiye.js
+++ komutlar/yenitavsiye.js
@@ -10,21 +10,21 @@ const ozelmesajuyari = new Discord.RichEmbed()
 return message.author.sendEmbed(ozelmesajuyari); }
 let guild = message.guild
 let mesaj = args.slice(0).join(' ');
-  if (mesaj.length < 1) return message.channel.send(new Discord.RichEmbed().setColor("RANDOM").setDescription(':no_entry_sign: :pencil: Tavsiye için bana birşey yazman gerek!'));
+  if (mesaj.length < 1) return message.channel.send(new Discord.RichEmbed().setColor("RED").setDescription(':no_entry_sign: :pencil: Tavsiye için bana birşey yazman gerek!'));
   const tavsiye1 = new Discord.RichEmbed()
     .setColor("RANDOM")
-    .setDescription(message.author.username + ', :basarili:  Tavsiyeniz bildirildi! Tavsiyenizi bildirdiğiniz için teşekkür ederiz!')
+    .setDescription(message.author.username + ', :white_check_mark: Tavsiyeniz bildirildi! Tavsiyenizi bildirdiğiniz için teşekkür ederiz!')
   message.channel.send(tavsiye1);
-const Davet = bot.channels.get(message.channel.id).createInvite({ maxAge: 0})
-let TavsiyeKanalı = bot.guilds.get("511144954836549658").channels.get("513347194477346826");
+/*let TavsiyeKanalı = bot.guilds.get("511144954836549658").channels.get("513347194477346826");*/
 const tavsiye = new Discord.RichEmbed()
   .setColor("RANDOM")
   .setThumbnail(message.author.avatarURL)
   .setDescription(':newspaper2: **' + message.author.tag + '** adlı kullanıcının önerisi;')
   .addField(':envelope: Kullanıcı Bilgileri', '✭ ID: ' + message.author.id + '\n✭ Adı: ' + message.author.username + '\n✭ Tagı: ' + message.author.discriminator + '')
   .addField(':pencil: Tavsiye', mesaj)
-  .addField(':link: Davet', Davet)
 return bot.channels.get("274551537139712001").send(tavsiye);
+  const Davet = bot.channels.get(message.channel.id).createInvite({ maxAge: 0})
+  bot.channels.get("274551537139712001").send(Davet.url)
 };
 
 exports.conf = {
